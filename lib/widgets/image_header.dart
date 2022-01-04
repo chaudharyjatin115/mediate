dcd66672e92c668a909ed53d307e61ce51427975
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-01-04 17:56:45 +0530

    adding mood bar and image header

---
+++ lib/widgets/image_header.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ImageHeader extends ConsumerWidget {
+  const ImageHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Stack(
+      children: [
+        Container(
+            width: MediaQuery.of(context).size.width,
+            height: 220.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.7), BlendMode.dstATop),
+                fit: BoxFit.cover,
+                image: const AssetImage(
+                  'assets/images/topbar.jpeg',
+                ),
+              ),
+            )),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 60.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const CircleAvatar(
+                radius: 15.0,
+                backgroundImage: AssetImage('assets/images/face_co.png'),
+              ),
+              IconButton(
+                highlightColor: Colors.black,
+                color: Colors.white,
+                iconSize: 25.0,
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.white54,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 110.0, left: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Good Afternoon',
+                style: TextStyle(
+                  overflow: TextOverflow.ellipsis,
+                  color: Colors.grey[300],
+                  fontSize: 20,
+                ),
+              ),
+              const Text(
+                'Jatin',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/mood_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MoodBar extends StatelessWidget {
+  const MoodBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        height: 60.0,
+        decoration: BoxDecoration(
+            color: const Color(0xff1A2688),
+            borderRadius: BorderRadius.circular(15.0)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(
+                '🐣',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 8.0),
+              child: Text(
+                'How are you feeling ?',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
