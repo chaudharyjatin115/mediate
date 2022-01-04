95f358a970a3a9792b95327284696e5f621a3625
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-01-04 17:58:19 +0530

    adding login screen

---
+++ lib/screens /login_scree.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Loginscreen extends ConsumerWidget {
+  const Loginscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(),
+      ),
+    );
+  }
+}
