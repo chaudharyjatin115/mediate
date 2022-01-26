b675040def5c3e57ee5f40e8b6d424e91bd19574
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-01-26 21:15:43 +0530

    adding some category and images

--- lib/data/data_private.dart
+++ lib/data/data_private.dart
@@ -1,3 +1,22 @@
 import 'package:mediate/models/models.dart';
 
-final audio1 = AudioCategory('assets/demo_1.png', 'calm', 5.5, 'Audio');
+final audio1 =
+    AudioCategory('assets/coverImages/cover_evening.jpg', 'calm', 5.5, 'Audio');
+final audio2 =
+    AudioCategory('assets/coverImages/flower_cover.jpg', 'calm', 5.5, 'Audio');
+final audio3 =
+    AudioCategory('assets/coverImages/japan_my.jpg', 'calm', 5.5, 'Audio');
+final audio4 =
+    AudioCategory('assets/coverImages/mountain.jpg', 'calm', 5.5, 'Audio');
+final audio5 =
+    AudioCategory('assets/coverImages/trees_Cover.jpg', 'calm', 5.5, 'Audio');
+final audio6 =
+    AudioCategory('assets/coverImages/way.jpg', 'calm', 5.5, 'Audio');
+final List<AudioCategory> audios = [
+  audio1,
+  audio2,
+  audio3,
+  audio4,
+  audio5,
+  audio6,
+];
--- lib/screens /home_screen.dart
+++ lib/screens /home_screen.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mediate/data/data_private.dart';
 import 'package:mediate/widgets/image_header.dart';
 import 'package:mediate/widgets/mood_bar.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
+    return Scaffold(body: Column(
+      children: [
+        Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: const [
@@ -22,11 +22,35 @@ class HomeScreen extends StatelessWidget {
                 'Recently Played',
                 style: TextStyle(color: Colors.white),
               ),
-
             ),
           ],
         ),
-      ),
-    );
+        Expanded(
+          child: ListView.builder(shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+            itemCount: audios.length,
+            
+            itemBuilder: (BuildContext context, int index) {
+              return Stack(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    child: const Text(
+                      'Hello',
+                    ),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(fit: BoxFit.cover,
+                        image: AssetImage('${audios[index].coverImage}'),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    ));
   }
 }
