048afdfa3115526fa43e05c6fbe872c61145ae14
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-01-29 19:57:53 +0530

    adding a playlist container

--- lib/screens /home_screen.dart
+++ lib/screens /home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mediate/data/data_private.dart';
+
 import 'package:mediate/widgets/image_header.dart';
 import 'package:mediate/widgets/mood_bar.dart';
 
@@ -8,68 +9,103 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(
+    return Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: const [
-            ImageHeader(),
-            MoodBar(),
-            Padding(
-              padding: EdgeInsets.only(left: 25),
-              child: Text(
-                'Recently Played',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ],
+        const ImageHeader(),
+        const MoodBar(),
+        const Padding(
+          padding: EdgeInsets.only(left: 25),
+          child: Text(
+            'Recently Played',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        const SizedBox(
+          height: 10.0,
         ),
         Expanded(
-          child: ListView.builder(shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
             itemCount: audios.length,
-            
             itemBuilder: (BuildContext context, int index) {
-              return Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 180,
-                      width: 150,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
+              return GestureDetector(onTap: (){},
+                child: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(7.0,7.0,7.0,0.0),
+                          child: Container(
+                            height: 130,
+                            width: 170,
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.only(topLeft:Radius.circular(8.0),
+                                  topRight: Radius.circular(8.0) ),
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('${audios[index].coverImage}'),
+                              ),
+                            ),
+                          ),
                         ),
-                        child: Text('${audios[index].name}'),
-                      ),
-                      decoration: BoxDecoration(
-                        image: DecorationImage(fit: BoxFit.cover,
-                          image: AssetImage('${audios[index].coverImage}'),
-
+                        Positioned(bottom: 20,
+                        left: 13,
+                          child: Container( height: 20,
+                          
+                          width: 80,
+                        
+                            child: Center(child: Text('${audios[index].name}',style: const TextStyle(color: Colors.white,),)),
+                            decoration: BoxDecoration(
+                              color: Colors.black12.withOpacity(0.6),
+                              borderRadius: BorderRadius.circular(10.0)
+                            ),
+                          ),
                         ),
-                        borderRadius: BorderRadius.circular(13.0),
-                          gradient: LinearGradient(
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                              stops: const [
-                                0.1,
-                                0.4,
-                                0.6,
-                                0.9
-                              ],
-                              colors: [
-                                Colors.black.withOpacity(0.3),
-                                Colors.black87.withOpacity(0.3),
-                                Colors.black54.withOpacity(0.3),
-                                Colors.black38.withOpacity(0.3)
-                              ])
-                      
-                      ),
+                       
+                        
+                      ],
                     ),
-                  ),
-                ],
+                     Container(
+                       height: 80,
+                    
+                       decoration:  BoxDecoration(
+                         border: Border.all(width: 0, color: Colors.black),
+                           color: const Color(0xff283a75),
+                           borderRadius: const BorderRadius.only(
+                               bottomLeft: Radius.circular(10.0),
+                               bottomRight: Radius.circular(10.0))),
+                       width: 170,
+                       child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                       mainAxisAlignment: MainAxisAlignment.center,
+                         children:  [
+                          const  Padding(
+                             padding: EdgeInsets.only(left: 10.0),
+                             child: Text(
+                               'Daily Calm',
+                               style: TextStyle(fontSize: 15.0,
+                                 color: Colors.white),
+                             ),
+                           ),
+                           const SizedBox(
+                             height: 2.0,
+                           ),
+                           Padding(
+                             padding: const EdgeInsets.only(left: 10.0),
+                             child:
+                           Text('${audios[index].length} mins',
+                            style: const TextStyle(color: Colors.white,
+                            fontSize: 12.0),
+                          ),)
+                         ],
+                       ),
+                     ),
+                      
+                 
+                  ],
+                ),
               );
             },
           ),
