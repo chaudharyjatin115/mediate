9479878ecbf246d2f5a36d50feaf19a2f763ba70
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-03-23 20:14:07 +0530

    adding linux support

--- lib/screens /on_boarding_screen.dart
+++ lib/screens /on_boarding_screen.dart
@@ -10,57 +10,59 @@ class OnBoardingScreen extends StatelessWidget {
       backgroundColor: const Color(0xff05164a),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Image(
-                height: 350,
-                image: AssetImage('assets/images/Meditation-working.jpg'),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 20.0),
-                child: Text(
-                  'Mediate',
-                  style: TextStyle(
-                      fontSize: 40.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Image(
+                  height: 350,
+                  image: AssetImage('assets/images/Meditation-working.jpg'),
                 ),
-              ),
-              const Text(
-                'Welcome to Mediate ',
-                style: TextStyle(color: Colors.white),
-              ),
-              const Text(
-                'A wonderful Streaming app with different ASMR ',
-                style: TextStyle(color: Colors.white),
-              ),
-              const Text(
-                'sounds to calm you through power of ASMR',
-                style: TextStyle(color: Colors.white),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              CustomLoginButton(
-                title: 'Get Started',
-                buttonColor: Colors.black12,
-                onTap: () {
-                  Navigator.pushNamed(context, 'SignUpScreen');
-                },
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              CustomLoginButton(
-                title: 'Log in',
-                buttonColor: Colors.black12,
-                onTap: () {
-                  Navigator.pushNamed(context, 'LoginScreen');
-                },
-              )
-            ],
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    'Mediate',
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Text(
+                  'Welcome to Mediate ',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const Text(
+                  'A wonderful Streaming app with different ASMR ',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const Text(
+                  'sounds to calm you through power of ASMR',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                CustomLoginButton(
+                  title: 'Get Started',
+                  buttonColor: Colors.black12,
+                  onTap: () {
+                    Navigator.pushNamed(context, 'SignUpScreen');
+                  },
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomLoginButton(
+                  title: 'Log in',
+                  buttonColor: Colors.black12,
+                  onTap: () {
+                    Navigator.pushNamed(context, 'LoginScreen');
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
