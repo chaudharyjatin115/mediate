5bded4bfb4ae411887ba5f68ae65fe700240e265
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-07-25 21:11:34 +0530

    fixing some  view constraints issues

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -28,86 +28,90 @@ class HomeScreen extends StatelessWidget {
                   BoxDecoration(borderRadius: BorderRadius.circular(10.0)),
 
             
-              child: ListTile(
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-              enabled: true,
-              onTap: () {},
-           
-              tileColor: const Color(0xff283a75),
-              leading: Container(
-                  padding: const EdgeInsets.only(right: 20, top: 10),
-                  height: 40,
-                width: 45,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(7.0),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                      image: AssetImage(audio1.coverImage!),
+              child: Expanded(
+                child: ListTile(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  enabled: true,
+                  onTap: () {},
+                  tileColor: const Color(0xff283a75),
+                  leading: Container(
+                    padding: const EdgeInsets.only(right: 20, top: 10),
+                    height: 40,
+                    width: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7.0),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(audio1.coverImage!),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    audio1.name!,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  trailing: IconButton(
+                    padding: const EdgeInsets.only(bottom: 10, right: 10),
+                    icon: const Icon(
+                      Icons.play_circle_fill,
+                      size: 40,
+                    ),
+                    onPressed: () {
+                      context.read<AudioPlayerBloc>().add(AudioPlayEventRemote(
+                            audioUrl: audio1.audioUrl!,
+                          ));
+                    },
+                    color: Colors.white,
                   ),
                 ),
               ),
-                title: Text(
-                  audio1.name!,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              trailing: IconButton(
-                padding: const EdgeInsets.only(bottom: 10, right: 10),
-                icon: const Icon(
-                  Icons.play_circle_fill,
-                    size: 40,
-                ),
-                  onPressed: () {
-                    context.read<AudioPlayerBloc>().add(AudioPlayEventRemote(
-                        audioUrl: audio1.audioUrl!, ));
-                  },
-                color: Colors.white,
-              ),
-            ),
           ),
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const ImageHeader(),
-            const MoodBar(),
-            const Padding(
-              padding: EdgeInsets.only(left: 25),
-              child: Text(
-                'Recently Played',
-                style: TextStyle(color: Colors.white),
+        body: ListView(children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ImageHeader(),
+              const MoodBar(),
+              const Padding(
+                padding: EdgeInsets.only(left: 25),
+                child: Text(
+                  'Recently Played',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 5.0,
-            ),
-            const RecentlyPlayed(),
-            const SizedBox(
-              height: 5.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Your Favourites',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.search,
-                      color: Colors.white,
+              const SizedBox(
+                height: 5.0,
+              ),
+              const RecentlyPlayed(),
+              const SizedBox(
+                height: 5.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Your Favourites',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ),
-                ],
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const FavouritesContainer(),
-          ],
-        ));
+              const FavouritesContainer(),
+            ],
+          ),
+        ]));
   }
 }
