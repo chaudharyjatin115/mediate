be810b30e9214c98b2db31f97136262f31f44b75
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-07-31 17:53:00 +0530

    handle errors

--- lib/services/audio_player_bloc/audio_player_bloc.dart
+++ lib/services/audio_player_bloc/audio_player_bloc.dart
@@ -7,12 +7,10 @@ import 'package:mediate/services/audio_player_bloc/audio_player_event.dart';
 import 'package:mediate/services/audio_player_bloc/audio_player_state.dart';
 
 class AudioPlayerBloc extends Bloc<AudioPlayerEvent, AudioPlayerState> {
-
   AudioPlayerBloc()
       : super(AudioPlayerInitialState(
             audio: audio1, audioPause: false, audioPlay: false)) {
     on<AudioPlayEventRemote>((event, emit) async {
-      
       try {
         audioPlayer1.setUrl(event.audio.audioUrl!);
         audioPlayer1.play();
@@ -32,15 +30,23 @@ class AudioPlayerBloc extends Bloc<AudioPlayerEvent, AudioPlayerState> {
           audioPlay: false, audioPause: false, audio: audio1));
     });
     on<AudioEventStop>((event, emit) async {
-     
-      audioPlayer1.pause();
-      print('audioPause');
-      emit(AudioPauseState(
-        audioPause: true,
-        audioPlay: false,
-        audio: event.audio,
-      ));
+      try {
+        audioPlayer1.pause();
+        print('audioPause');
+        emit(AudioPauseState(
+          audioPause: true,
+          audioPlay: false,
+          audio: event.audio,
+        ));
+      } catch (e) {
+        emit(AudioPlayerErrorState(
+            error: e.toString(),
+            audioPlay: false,
+            audioPause: true,
+            audio: event.audio));
+      }
     });
   }
 }
+
 final audioPlayer1 = AudioPlayer();
